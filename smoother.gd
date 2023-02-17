3ff8a14a808aab877d3a67f438fbdbbdd89b6056
Anatol Bogun <anatol.bogun@3plearning.com>
2023-02-17 10:29:17 +0900

    fix breaking release candidate type safety changes

--- smoother.gd
+++ smoother.gd
@@ -21,7 +21,7 @@
 class_name Smoother extends Node
 
 ## Smoother Node
-## Version: 1.0.2
+## Version: 1.0.3
 ##
 ## A node type that smoothes scene nodes' properties by interpolating _physics_process steps.
 ##
@@ -202,11 +202,13 @@ func _physics_process(_delta: float) -> void:
 
 ## Get the relevant nodes to be smoothed based on this node's tree position and properties.
 func _get_physics_process_nodes(node: Node, ignore_node: = false, with_includes: = true) -> Array[Node]:
-	var nodes:Array[Node] = includes.map(
+	var nodes:Array[Node]
+
+	nodes.assign(includes.map(
 		get_node_or_null
 	).filter(
 		func (node:Node) -> bool: return node != null && !excludes.has(get_path_to(node))
-	) as Array[Node] if with_includes else [] as Array[Node]
+	) if with_includes else [])
 
 	if (
 		!ignore_node
